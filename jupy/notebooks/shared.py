8dc50283da8c2eac36e0422bf5bcc25978792f7a
Sasha Karalchuk <sasha.karalchuk@gmail.com>
2024-03-11 15:52:00 +0100

    chore: add temporary calcs

--- jupy/notebooks/shared.py
+++ jupy/notebooks/shared.py
@@ -1,10 +1,37 @@
+"""
+!pip install pandas mplfinance pandera
+"""
+
 import os
+import typing
 import logging
 import importlib
 import datetime as dt
 
+import sqlalchemy.orm
+import sqlalchemy as sa
 import pandas as pd
-from binance_historical_data import BinanceDataDumper
+import mplfinance as mpf
+
+
+interval_secs_map = {
+    '1m': 60,
+    '5m': 5*60,
+    '15m': 15*60,
+    '1h': 60*60,
+    '4h': 4*60*60,
+    '1d': 24*60*60,
+    '1w': 7*24*60*60,
+}
+s  = mpf.make_mpf_style(
+    marketcolors=mpf.make_marketcolors(
+        up='#459782',
+        down='#df484c',
+        edge='inherit',
+        wick='inherit',
+    ),
+    facecolor='#181b25',
+)
 
 
 def get_logger() -> logging.RootLogger:
@@ -31,33 +58,60 @@ def read_matic_k_lines() -> pd.DataFrame:
     return matic_df
 
 
-def dump_binance_klines(tickers: list[str]) -> None:
-    """Downloads binance klines into files.
-    
-    Examples:
-    dump_binance_klines(tickers=['BTCUSDT'])
+def read_binance_klines() -> pd.DataFrame:
+    """Examples:
+    btc_df = shared.read_binance_ticker_data()
     """
-    data_dumper = BinanceDataDumper(
-        path_dir_where_to_dump="/home/jovyan/.var/binance",
-        asset_class="spot",
-        data_type="klines",
-        data_frequency="1m",
-    )
-    data_dumper.dump_data(
-        tickers=tickers,
-        date_start=dt.date(year=2024, month=1, day=1),
-        date_end=dt.date(year=2024, month=2, day=1),
-        is_to_update_existing=True,
+    dir_path = '/home/jovyan/.var/binance-local/spot/klines/1m/BTCUSDT'
+    file_names = os.listdir(dir_path)
+    dfs = [pd.read_csv(f'{dir_path}/{file_name}') for file_name in file_names]
+    return (pd.concat(dfs, ignore_index=True)
+        .assign(date=lambda x: x.open_time.apply(lambda x: dt.datetime.fromtimestamp(x/1000)))
+        .set_index('date')
     )
 
 
-def read_binance_ticker_data() -> pd.DataFrame:
-    columns = ['open_time', 'open', 'high', 'low', 'close', 'volume',
-               'kline_close_time', 'quote_asset_volume', 'number_of_trades',
-               'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
-               't']
-    file_path = '/home/jovyan/.var/binance/spot/monthly/klines/BTCUSDT/1m/BTCUSDT-1m-2024-01.csv'
-    btc_df = (pd.read_csv(file_path, header=None, names=columns)
-        .assign(_open_time=lambda x: x.open_time.apply(lambda x: dt.datetime.fromtimestamp(x/1000)))
+def read_binance_trades() -> pd.DataFrame:
+    file_path = '/home/jovyan/.var/binance/spot/monthly/trades/BTCUSDT/BTCUSDT-trades-2024-01.csv'
+    columns = ['id', 'price', 'qty', 'base_qty', 'time', 'is_buyer', 'is_maker']
+    return (pd.read_csv(file_path, names=columns, header=None, nrows=1_000_000)
+        .assign(
+            date=lambda x: x.time.apply(lambda x: dt.datetime.fromtimestamp(x/1000)),
+            open=lambda x: x.price,
+            close=lambda x: x.price,
+            low=lambda x: x.price,
+            high=lambda x: x.price,
+        )
+        .set_index('date')
     )
-    return btc_df
+
+
+def split_df(df: pd.DataFrame, interval_str: str) -> pd.DataFrame:
+    """Examples:
+    btc_df = shared.read_binance_klines()
+    btc_15m_df = shared.split_df(df=btc_df, interval_str='15m')
+    """
+    interval_millis = interval_secs_map[interval_str] * 1000
+    logger = get_logger()
+    prev_millis, prev_i = df.open_time.min(), 0
+    out_dfs = []
+    for i in range(len(df)):
+        if df.iloc[i].open_time < prev_millis+interval_millis:
+            continue
+        slice_df = df.iloc[prev_i:i].assign(open_time=lambda _: prev_millis)
+        out_dfs.append(slice_df)
+        prev_millis, prev_i = df.iloc[i].open_time, i
+    logger.info('len(out_dfs): %s', len(out_dfs))
+    out_df = (pd.concat(out_dfs)
+        .groupby(by=['open_time'], as_index=False)
+        .agg(
+            open=('open', 'first'),
+            close=('open', 'last'),
+            low=('low', 'min'),
+            high=('high', 'max'),
+            volume=('volume', 'sum'),
+        )
+        .assign(date=lambda x: x.open_time.apply(lambda x: dt.datetime.fromtimestamp(x/1000)))
+        .set_index('date')
+    )
+    return out_df
