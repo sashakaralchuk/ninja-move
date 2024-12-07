2d4111b8a6d9c3f7887d01a9716d36a9885f4e77
Sasha Karalchuk <sasha.karalchuk@gmail.com>
2024-12-07 11:54:44 +0100

    chore: move clients code to separate file

--- core-cpp/src/clients.hpp
+++ core-cpp/src/clients.hpp
@@ -28,3 +28,117 @@ class ClientPublic : public hv::WebSocketClient {
     virtual void handle_onmessage(const std::string& msg) = 0;
     void init_idle_(std::string& url);
 };
+
+class ClientPublicGateio : public ClientPublic {
+   public:
+    ClientPublicGateio(std::string kind);
+
+    void init_idle();
+
+    void subscribe_to_trades(std::string& symbol);
+
+    void subscribe_to_depth(std::string& symbol);
+
+    void unsubscribe_from_depth(std::string& symbol);
+
+    void ping();
+
+    std::vector<Depth> fetch_depth_snapshot(std::string& symbol);
+
+   protected:
+    void handle_onmessage(const std::string& msg);
+};
+
+class ClientPublicMexc : public ClientPublic {
+   public:
+    ClientPublicMexc(std::string kind);
+
+    void init_idle();
+
+    void subscribe_to_trades(std::string& symbol);
+
+    void subscribe_to_depth(std::string& symbol);
+
+    void unsubscribe_from_depth(std::string& symbol);
+
+    void ping();
+
+    std::vector<Depth> fetch_depth_snapshot(std::string& symbol);
+
+   protected:
+    void handle_onmessage(const std::string& msg);
+
+   private:
+    bool subscribed_to_depth = false;
+};
+
+class ClientPrivateMexc : public ClientPublic {
+   public:
+    ClientPrivateMexc(std::string kind);
+
+    void init_idle_private(std::string& listen_key);
+
+    void subscribe_to_private_events();
+
+    void place_spot_limit_order();
+
+    static void place_fut_limit_order();
+
+    std::string create_listen_key();
+
+   private:
+    std::string api_key;
+    std::string api_secret;
+    std::string sign_str(std::string& query);
+};
+
+class ClientPublicBybit : public ClientPublic {
+   public:
+    ClientPublicBybit(std::string kind);
+
+    void init_idle();
+
+    void subscribe_to_trades(std::string& symbol);
+
+    void subscribe_to_depth(std::string& symbol);
+
+    void unsubscribe_from_depth(std::string& symbol);
+
+    void ping();
+
+   protected:
+    void handle_onmessage(const std::string& msg);
+
+   private:
+    Depth parse_depth(nlohmann::json& msg_obj);
+};
+
+class ClientPublicHtx : public ClientPublic {
+   public:
+    ClientPublicHtx(std::string kind);
+
+    void init_idle();
+
+    void subscribe_to_trades(std::string& symbol);
+
+    void subscribe_to_depth(std::string& symbol);
+
+    void unsubscribe_from_depth(std::string& symbol);
+
+    void ping();
+
+    static std::string parse_symbol_from_trade_ch(std::string& ch);
+
+    static std::string parse_symbol_from_depth_ch(std::string& ch);
+
+    static int gzDecompress(const char* src, int srcLen, const char* dst,
+                            int dstLen);
+
+    std::vector<Depth> fetch_depth_snapshot(std::string& symbol);
+
+   protected:
+    void handle_onmessage(const std::string& msg);
+
+   private:
+    std::vector<Depth> parse_depth_from_res(std::string s);
+};
